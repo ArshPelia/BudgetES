c0ddac6118f044fb47b22d2ae005c49e497ed037
arshjit pelia <arsh.pelia1@gmail.com>
2023-09-02 16:54:31 -0400

    updated user model

--- ES/ESapp/models.py
+++ ES/ESapp/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class User(AbstractUser):
     # Any custom fields you might want to add to the user model
+    age = models.IntegerField(default=None,null=True)
+    ret_fund = models.DecimalField(default= None, max_digits=10, decimal_places=2, null=True)
+    emg_fund = models.DecimalField(default= None, max_digits=10, decimal_places=2, null=True)
+    newuser = models.BooleanField(default=True)
     pass
