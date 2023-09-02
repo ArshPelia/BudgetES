581dbbe1a4f8fddcf26b8b7f86dfb8b26c491d93
arshjit pelia <arsh.pelia1@gmail.com>
2023-09-02 16:08:16 -0400

    initialized the django website

---
+++ ES/ESapp/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    # Any custom fields you might want to add to the user model
+    pass
---
+++ ES/ESapp/static/ESapp/index.js
@@ -0,0 +1,10 @@
+/* when the DOM content of the page has been loaded, 
+  we attach event listeners to each of the buttons. 
+  
+  Ensures use of the function to only run the code once all content has loaded
+   
+   */
+// Wait for the DOM content to load before attaching event listeners
+document.addEventListener('DOMContentLoaded', function() {
+
+});
